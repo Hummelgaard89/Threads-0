d49dd14cc65b1351e439239d7eebe8882961a4ad
caspe <caspe@DESKTOP-H8GT2JR>
2021-11-01 17:22:41 +0100

    Add project files.

---
+++ Threads 0/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads_0
+{
+    
+    class threprog
+    {
+        public static void Main()
+        {
+        Program pg = new Program();
+            Thread thread0 = new Thread(pg.WorkThreadFunction);
+            Thread thread1 = new Thread(pg.WorkThreadFunction);
+            thread0.Start("C#-trådning er nemt!");
+            thread0.Join();
+            thread1.Start("Også med flere tråde...");
+            thread1.Join();
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        public void WorkThreadFunction(object threadname)
+        {
+            string tn = threadname as string;
+            for (int i = 0; i < 5; i++)
+            {
+                if (tn != null)
+                {
+                Console.WriteLine(tn);
+                    Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
